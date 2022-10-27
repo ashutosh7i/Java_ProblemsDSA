bc0aa5dd594b9a9abcf5b40b1641e0f0bd8d7f07
Aashutosh Soni <ashutoshsoni130@gmail.com>
2022-10-27 18:47:44 +0530

    CountTriangel

---
+++ Concepts/Patterns/P7.java
@@ -0,0 +1,19 @@
+//Comment Here Chad😂😉
+import java.util.Scanner;
+
+public class P7{
+   public static void main (String[] args){
+       //Scanner for input🤖
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+
+       int count = 1;
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(count);
+            count++;
+        }
+        System.out.println();
+       }
+    }
+}
