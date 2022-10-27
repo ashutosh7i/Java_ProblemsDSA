c5f910d8dd0cbaf4ba99fcb8c4e596ee0aea5dc8
Aashutosh Soni <ashutoshsoni130@gmail.com>
2022-10-27 17:11:43 +0530

    new file:   Concepts/Patterns/P4.class

---
+++ Concepts/Patterns/P4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class P4{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    int count=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            System.out.print(" "+count);
+            count++;
+        }
+    System.out.println();
+    }
+
+        
+    }
+}
