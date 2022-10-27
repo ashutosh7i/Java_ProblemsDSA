19629b783603a517552ba8d3b36270822af204b5
Aashutosh Soni <ashutoshsoni130@gmail.com>
2022-10-27 16:58:24 +0530

    Pattern Problems

---
+++ Concepts/Patterns/P3.java
@@ -0,0 +1,20 @@
+// 1234
+// 1234
+// 1234
+// 1234
+
+import java.util.Scanner;
+public class P3{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++){
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+
+        
+    }
+}
---
+++ Concepts/Patterns/P3Reverse.java
@@ -0,0 +1,20 @@
+// 321
+// 321
+// 321
+import java.util.Scanner;
+public class P3Reverse{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    for (int i=n;i>=1;i--){
+        for (int j=n;j>=1;j--){
+            System.out.print(" "+j);
+        }
+        System.out.println();
+    }
+    
+
+        
+    }
+}
